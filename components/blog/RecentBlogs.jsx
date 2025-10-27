39cc80fab481378487f2dc507c9069598dcb3c61
Joylinton04 <adujoy05@gmail.com>
2025-10-27 22:36:06 +0000

    refactor: adjusted the gap between the blog cards

--- components/blog/RecentBlogs.jsx
+++ components/blog/RecentBlogs.jsx
@@ -7,7 +7,7 @@ const RecentBlogs = () => {
         <div className='md:px-page-x lg:py-page-y px-page-sx font-poppins my-16'>
             <div className='max-w-7xl mx-auto flex flex-col gap-6'>
                 <h1 className='font-open_sans text-2xl font-bold'>Recent Blog Posts</h1>
-                <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-cols-1 my-10 gap-x-20'>
+                <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-cols-1 my-10 gap-x-20 gap-y-10'>
                     <BlogCard />
                     <BlogCard />
                     <BlogCard />
