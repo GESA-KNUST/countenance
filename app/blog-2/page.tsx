444d0dd8520c24396c6d08889ad8feeb940be2ce
Obrempong-1 <obrempong.kow@gmail.com>
2025-12-03 18:43:48 +0000

    refactored blog-2

--- app/blog-2/page.tsx
+++ app/blog-2/page.tsx
@@ -1,38 +1,11 @@
 'use client';
-import { useState, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
-import MainContent from '../../components/blog-2/MainContent';
-import PopularPosts from '../../components/blog-2/PopularPosts';
-import RecentBlogs from '../../components/blog/RecentBlogs';
-import useBlogCollection from '../../hooks/useBlogCollection';
+import { Suspense } from 'react';
+import BlogContent from './BlogContent';
 
 export default function Page() {
-  const { data: allPosts } = useBlogCollection();
-  const [selectedPost, setSelectedPost] = useState(null);
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const slug = searchParams.get('slug');
-    if (slug && allPosts) {
-      const post = allPosts.find((p) => p.slug === slug);
-      if (post) {
-        setSelectedPost(post);
-      }
-    }
-  }, [searchParams, allPosts]);
-
-  const handlePostSelect = (post) => {
-    setSelectedPost(post);
-  };
-
   return (
-    <main className="flex min-h-screen flex-col items-center bg-white font-poppins">
-      <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 md:px-2 lg:px-8 pt-24 pb-16">
-        <div className="flex flex-col lg:flex-row gap-16">
-          <MainContent selectedPost={selectedPost || (allPosts && allPosts[0])} />
-          <PopularPosts allPosts={allPosts} onPostSelect={handlePostSelect} />
-        </div>
-      </div>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BlogContent />
+    </Suspense>
   );
 }
